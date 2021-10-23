d4b0d276bf854666da1b00a17bd75c92ffa19aa5
Miro778 <83011341+Miro778@users.noreply.github.com>
2021-10-23 19:21:41 +0300

    Vuoroja voi nyt lisätä työvuorojen listaan sovelluksesta

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@mui/styles'
 import Modal from '@material-ui/core/Modal'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import { white } from '@material-ui/core/styles/colors'
 
 const App = () => {
 
@@ -20,6 +19,114 @@ const App = () => {
   const [yolisaAlku, setYolisaAlku] = useState(22)
   const [yolisaLoppu, setYolisaLoppu] = useState(7)
 
+  function VuoroLisaysModal() {
+
+    function getModalStyle() {
+      const top = 50
+      const left = 50
+  
+      return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+      }
+    }
+  
+    const classesModal = modalStyles()
+    const [modalStyle] = React.useState(getModalStyle)
+    const [open, setOpen] = React.useState(false)
+    const [alkuPvm, setAlkuPvm] = useState('01/12/2021')
+    const [loppuPvm, setLoppuPvm] = useState('01/12/2021')
+    const [alkuKlo, setAlkuKlo] = useState('07:00')
+    const [loppuKlo, setLoppuKlo] = useState('15:00')
+  
+    const handleOpen = () => {
+      setOpen(true)
+    }
+  
+    const handleClose = () => {
+      setOpen(false)
+    }
+  
+    const lisaaVuoro = (event) =>  {
+      event.preventDefault()
+  
+      const uusiVuoro = {
+        alkuPvm: alkuPvm,
+        loppuPvm: loppuPvm,
+        alkuKlo: alkuKlo,
+        loppuKlo: loppuKlo
+      }
+  
+      console.log('Lisätään vuoro: ' , uusiVuoro)
+      tyovuorot.push(uusiVuoro)
+      console.log('Vuorot: ' , tyovuorot)
+    }
+  
+    return (
+      <div>
+        <Button color="primary" variant="contained" onClick={handleOpen}>+ Lisää työvuoro</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div style={modalStyle} className={classesModal.paper}>
+            <form onSubmit={lisaaVuoro}>
+              <h2 id="simple-modal-title">Ilmoita työvuoron alku- ja loppuaika</h2>
+              <p>Pvm muodossa pv/kk/vuosi, ja kellonajat muodossa 00:00</p>
+              <div>
+                <TextField label="Alkupvm"
+                  id='alkuPvmField'
+                  type="text"
+                  value={alkuPvm}
+                  name="Title"
+                  size='small'
+                  required={true}
+                  onChange={({ target }) => setAlkuPvm(target.value)}
+                />
+                <span> </span>
+                <TextField label="Alkaa klo:"
+                  id='alkuKloField'
+                  type="text"
+                  value={alkuKlo}
+                  name="Title"
+                  size='small'
+                  required={true}
+                  onChange={({ target }) => setAlkuKlo(target.value)}
+                />
+                <br />
+                <TextField label="Loppupvm"
+                  id='loppuPvmField'
+                  type="text"
+                  value={loppuPvm}
+                  name="Title"
+                  size='small'
+                  required={true}
+                  onChange={({ target }) => setLoppuPvm(target.value)}
+                />
+                <span> </span>
+                <TextField label="Loppuu klo:"
+                  id='loppuKloField'
+                  type="text"
+                  value={loppuKlo}
+                  name="Title"
+                  size='small'
+                  required={true}
+                  onChange={({ target }) => setLoppuKlo(target.value)}
+                />
+              </div>
+              <div>
+                <Button variant="contained" color="primary" id='lisaaVuoro-button' type="submit">Lisää</Button>
+              </div>
+            </form>
+          </div>
+        </Modal>
+      </div>
+    )
+  }
+
 
   return (
 <body id='body'>
@@ -32,31 +139,31 @@ const App = () => {
   <h2>Palkkatiedot</h2>
   <form>
   <div id='syottokentta'>
-  <label for="ppalkka">Perustuntipalkka:</label>
+  <label htmlFor="ppalkka">Perustuntipalkka:</label>
   <input type="text" id="ppalkka" size="1" name="perustuntipalkka" value={perusPalkka} onChange={({ target }) => setPerusPalkka(target.value)}/>
   <span>€/h</span>
   </div>
   <br />
   <div id='syottokentta'>
-  <label for="ilisa">Iltalisä:</label>
+  <label htmlFor="ilisa">Iltalisä:</label>
   <input type="text" id="ilisa" size="1" name="iltalisa" value={iltalisa} onChange={({ target }) => setIltalisa(target.value)}/>
   <span>€/h, </span>
-  <label for="ylisa">Yölisä:</label>
+  <label htmlFor="ylisa">Yölisä:</label>
   <input type="text" id="ylisa" size="1" name="yolisa" value={yolisa} onChange={({ target }) => setYolisa(target.value)}/>
   <span>€/h, </span>
-  <label for="llisa">Lauantailisä:</label>
+  <label htmlFor="llisa">Lauantailisä:</label>
   <input type="text" id="llisa" size="1" name="lauantailisa" value={lauantailisa} onChange={({ target }) => setLauantailisa(target.value)}/>
   <span>€/h</span>
   </div>
   <br />
   <div id='syottokentta'>
-  <label for="ilisaAlku">Iltalisä masketaan jälkeen klo:</label>
+  <label htmlFor="ilisaAlku">Iltalisä masketaan jälkeen klo:</label>
   <input type="text" id="ilisaAlku" size="1" name="ilisaAlku" value={iltalisaAlku} onChange={({ target }) => setIltalisaAlku(target.value)}/>
   <span>: 00, </span>
-  <label for="ylisaAlku">Yölisä masketaan jälkeen klo:</label>
+  <label htmlFor="ylisaAlku">Yölisä masketaan jälkeen klo:</label>
   <input type="text" id="ylisaAlku" size="1" name="ylisaAlku" value={yolisaAlku} onChange={({ target }) => setYolisaAlku(target.value)}/>
   <span>: 00, </span>
-  <label for="ylisaLoppu">Yölisä loppuu klo:</label>
+  <label htmlFor="ylisaLoppu">Yölisä loppuu klo:</label>
   <input type="text" id="ylisaLoppu" size="1" name="ylisaLoppu" value={yolisaLoppu} onChange={({ target }) => setYolisaLoppu(target.value)}/>
   <span>: 00</span>
   </div>
@@ -77,70 +184,6 @@ const modalStyles = makeStyles((theme) => ({
     border: '2px solid #000',
   },
 }))
-
-function VuoroLisaysModal() {
-
-  function getModalStyle() {
-    const top = 50
-    const left = 50
-
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    }
-  }
-
-  const classesModal = modalStyles()
-  const [modalStyle] = React.useState(getModalStyle)
-  const [open, setOpen] = React.useState(false)
-  const [alkuPvm, setAlkuPvm] = useState('')
-  const [loppuPvm, setLoppuPvm] = useState('')
-  const [alkuKlo, setAlkuKlo] = useState('')
-  const [loppuKlo, setLoppuKlo] = useState('')
-
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-
-  const lisaaVuoro = () =>  {
-    console.log('todo')
-  }
-
-  return (
-    <div>
-      <Button color="primary" variant="contained" onClick={handleOpen}>+ Lisää työvuoro</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <div style={modalStyle} className={classesModal.paper}>
-          <form onSubmit={lisaaVuoro}>
-            <h2 id="simple-modal-title">Ilmoita työvuoron alku- ja loppuaika</h2>
-            <div>
-              <TextField label="Alkupvm"
-                id='alkuPvmField'
-                type="text"
-                value={alkuPvm}
-                name="Title"
-                onChange={({ target }) => setAlkuPvm(target.value)}
-              />
-            </div>
-            <div>
-              <Button variant="contained" color="primary" id='submitAvatar-button' type="submit">Submit</Button>
-            </div>
-          </form>
-        </div>
-      </Modal>
-    </div>
-  )
-}
 
 
 export default App
